bc8b337c30b3fc8ea2b7e1a06502262efd62bef4
Edgars <edgarskakarans@gmail.com>
2021-11-02 14:40:57 +0200

    Switched to PostgreSQL + small changes here and there

--- cars_test/app/models/car.rb
+++ cars_test/app/models/car.rb
@@ -1,5 +1,10 @@
 class Car < ApplicationRecord
 
+    validates :make, presence: true
+    validates :fuel, presence: true
+    validates :inspection, presence: true
+    validates :score, presence: true
+
     def self.populate_database
 
         path = "/home/me/Ruby On Rails/rails-treinings-data/test_data/sample_ta2020.txt"
@@ -11,13 +16,17 @@ class Car < ApplicationRecord
         #populates also invalid objects -without value fields, needs validations
         File.open(path).each_with_index do |line, index|
             next if index == 0                                     
-            args = line.split(',')                                                
-            car_to_save = Car.new
-            car_to_save.make = args[1]
-            car_to_save.fuel = args[2]
-            car_to_save.inspection = args[3]
-            car_to_save.score = args[4]
-            car_to_save.save
+            args = line.split(',')   
+
+            car_to_save = Car.new({
+            make: args[1].strip,
+            fuel: args[2].strip,
+            inspection: args[3].strip,
+            score: args[4].strip
+            })
+
+            car_to_save.save if car_to_save.valid?
+            # could log or count how many records were not saved
         end
     end
 
