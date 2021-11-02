724d8267dffad742aa3367305c63bf1416a3f7c6
Edgars <edgarskakarans@gmail.com>
2021-11-02 16:14:26 +0200

    Created case insensitive search - data stored in upcase, entered params before search are upcased

--- cars_test/app/controllers/cars_controller.rb
+++ cars_test/app/controllers/cars_controller.rb
@@ -5,6 +5,16 @@ class CarsController < ApplicationController
 
     def search
         @search_string = params.inspect
+
+        if params[:make].present? && params[:model].present? && params[:fuel].present?
+            @list = Car.search(params)
+        else
+            
+            flash.alert = "Jāaizpilda visi lauciņi!"
+            render 'index'
+            
+        end
+
     end
 
     def list
--- cars_test/app/models/car.rb
+++ cars_test/app/models/car.rb
@@ -7,7 +7,7 @@ class Car < ApplicationRecord
 
     def self.populate_database
 
-        path = "/home/me/Ruby On Rails/rails-treinings-data/test_data/sample_ta2020.txt"
+        path = "/home/me/Ruby On Rails/rails-treinings-data/test_data/ta2020e.lst"
 
         if File.exists?(path)
             puts "File found"
@@ -15,14 +15,15 @@ class Car < ApplicationRecord
 
         #populates also invalid objects -without value fields, needs validations
         File.open(path).each_with_index do |line, index|
-            next if index == 0                                     
+            next if index == 0 
+            break if index == 10000 #limiting amount of data thats been read                                    
             args = line.split(',')   
 
             car_to_save = Car.new({
-            make: args[1].strip,
-            fuel: args[2].strip,
-            inspection: args[3].strip,
-            score: args[4].strip
+            make: args[1].strip.upcase,
+            fuel: args[2].strip.upcase,
+            inspection: args[3].strip.upcase,
+            score: args[4].strip.upcase
             })
 
             car_to_save.save if car_to_save.valid?
@@ -30,12 +31,10 @@ class Car < ApplicationRecord
         end
     end
 
-
-
     def self.search(params)
-        Car.where('make LIKE ? AND make LIKE ? AND fuel LIKE ?', "%#{params[:make]}%", "%#{params[:model]}%", "%#{params[:fuel]}%")
+        Car.where('make LIKE ? AND make LIKE ? AND fuel LIKE ?', 
+            "%#{params[:make].upcase}%", "%#{params[:model].upcase}%", "%#{params[:fuel].upcase}%")
     end
-
 
 
 end
