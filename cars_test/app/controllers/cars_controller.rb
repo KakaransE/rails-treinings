61332af26cf7f181184c79a035e4b42ec287f60f
Edgars <edgarskakarans@gmail.com>
2021-11-05 09:55:54 +0200

    Cleaned up a bit controllers. After notifications stay in the same page.

--- cars_test/app/controllers/cars_controller.rb
+++ cars_test/app/controllers/cars_controller.rb
@@ -3,33 +3,15 @@ class CarsController < ApplicationController
     def index
     end
 
-    # def search
-    #     @search_string = params.inspect 
-        
-    #     if params[:make].present? && params[:model].present? && params[:fuel].present? && (params[:only_make].to_i == 0)
-    #         @list = Car.search(params)
-    #         @average = Car.weighted_average(@list)
-    #         @graph_data_basic = Car.graph_data_basic(@list)
-    #         @graph_data_repeated = Car.graph_data_repeated(@list)
-    #         @chart_max = Car.chart_max(@graph_data_basic)
-    #         @thumbnail_url = ImageScraper.get_image(params[:make], params[:model])
-    #     else
-            
-    #         flash.now[:danger] = "Jāaizpilda visi lauciņi!"
-    #         render 'index'
-            
-    #     end
-
-    # end
-
     def search
+
+        # Should probably create search object and pass params to it and make validations etc. in it, - would help with this clutter
         # @search_string = params.inspect # for troubleshooting
 
         if params[:make].present? && (params[:only_make].to_i == 1)
             @list = Car.search_make(params)
             if  @list.count == 0
-                flash.now[:danger] = "Nekas netika atrasts!"
-                render 'index'
+                redirect_to request.referrer, notice: "Nekas netika atrasts!"
             else
                 params[:model] = "visi"
                 params[:fuel] = "visi"
@@ -42,8 +24,7 @@ class CarsController < ApplicationController
         elsif params[:make].present? && params[:model].present? && params[:fuel].present? && (params[:only_make].to_i == 0)
             @list = Car.search(params)
             if  @list.count == 0
-                flash.now[:danger] = "Nekas netika atrasts!"
-                render 'index'
+                redirect_to request.referrer, notice: "Nekas netika atrasts!"
             else
                 @average = Car.weighted_average(@list)
                 @graph_data_basic = Car.graph_data_basic(@list)
@@ -52,10 +33,8 @@ class CarsController < ApplicationController
                 @thumbnail_url = ImageScraper.get_image(params[:make], params[:model])
             end
         else
-            
-            flash.now[:danger] = "Jāaizpilda visi atlases kritēriju lauki, vai jāievada auto ražotājs un jāatzīmē kritērijs: Atlasīt tikai pēc auto ražotāja!"
-            render 'index'
-            
+            redirect_to request.referrer, 
+                notice: "Jāaizpilda visi atlases kritēriju lauki, vai jāievada auto ražotājs un jāatzīmē kritērijs: Atlasīt tikai pēc auto ražotāja!"
         end
 
     end
--- cars_test/app/controllers/pages_controller.rb
+++ cars_test/app/controllers/pages_controller.rb
@@ -8,6 +8,5 @@ class PagesController < ApplicationController
 
     def about
     end
-    
 
 end
